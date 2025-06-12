0e0db2031e114430d2c44428e768c10933f3daff
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-12 10:51:05 +0200

    add button to switch to dark mode

--- js-basic/js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
+++ js-basic/js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,11 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+// btn für Dark Mode (add)
+
+const btnDarkMode = document.querySelector(`[data-js="dark-mode-button"]`);
+
+btnDarkMode.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
