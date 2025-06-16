435847edfeb90c849cf82209a961db28ebd5a41b
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-16 12:24:26 +0200

    formula for square + formula for circumference

--- js-basic/js-functions/js-functions_functions-with-parameters-extra/js/index.js
+++ js-basic/js-functions/js-functions_functions-with-parameters-extra/js/index.js
@@ -6,7 +6,10 @@ console.clear();
    The formula is: square = number * number
 */
 
-function printSquare(number) {}
+function printSquare(number) {
+  const square = number * number;
+  console.log(square);
+}
 
 printSquare(3);
 printSquare(5);
@@ -19,10 +22,16 @@ printSquare(5);
 
 // Uncomment the following function calls and implement the function `printCircumference`.
 
-// printCircumference(4);
-// printCircumference(6);
+function printCircumference(radius) {
+  const circumference = 2 * Math.PI * radius;
+  console.log(circumference);
+}
+
+printCircumference(4);
+printCircumference(6);
 
 /*
+
 3: Write a function that accepts the width and length of a rectangle
    and logs the following text to the console: "The area of the rectangle is ?".
    The function should replace the question mark "?" with the correct area.
