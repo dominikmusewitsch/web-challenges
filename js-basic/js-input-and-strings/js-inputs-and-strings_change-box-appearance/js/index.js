6a7122c61ab4efe1b35775722a6b4116acfdbf3f
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-17 12:13:51 +0200

    add variables to ranges

--- js-basic/js-input-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-basic/js-input-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,13 @@
 console.clear();
+
+const colorRange = document.querySelector('[data-js="input-color"]');
+const borderRadiusRange = document.querySelector('[data-js="input-radius"]');
+const rotationRadius = document.querySelector('[data-js="input-rotation"]');
+
+// Change the appearance of the box according to the slider values (color, border radius, and rotation).
+
+// [ ] Add an event listener for each input element.
+// [ ] When the slider control of inputColor is moved, change the background color of the box. Hint: Use the hsl() function to set the color dynamically.
+// [ ] When the slider control of inputRadius is moved, change the border radius of the box. If the slider is at its maximum, make the box circular.
+// [ ] When the slider control of inputRotation is moved, rotate the box. Hint: Use the transform CSS property with a rotate value in deg.
+// Hint: You can modify styles directly by accessing the style property of the box, similar to how you work with classList.
