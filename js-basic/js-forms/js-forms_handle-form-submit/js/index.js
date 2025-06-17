f53a7ded8065c78bf29afe4a54d4ea262640e34f
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-17 15:28:47 +0200

    setup js to react to form submission

--- js-basic/js-forms/js-forms_handle-form-submit/js/index.js
+++ js-basic/js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,27 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+});
+
+// Use JavaScript to react to the form submission.
+
+// Log all form data (in object form) into the console in the submit event handler.
+
+// Make sure the form is reset and the First Name input is focused after submission.
+
+// 💡 You only have to touch the js/index.js file.
+
+// Bonus: Calculate and log the age-badness-sum 🤷‍♀️
+// The formula is: age + badness
+
+// Log to the console (replace the values between the quotes):
+
+// The age-badness-sum of "firstName" is "age-badness-sum"
