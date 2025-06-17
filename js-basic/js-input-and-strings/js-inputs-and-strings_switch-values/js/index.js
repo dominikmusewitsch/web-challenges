286edf3476bd172de1ca33cdfe40aa3a6e539b42
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-17 11:07:04 +0200

    assign switch button to variable

--- js-basic/js-input-and-strings/js-inputs-and-strings_switch-values/js/index.js
+++ js-basic/js-input-and-strings/js-inputs-and-strings_switch-values/js/index.js
@@ -5,6 +5,8 @@ const firstInput = document.querySelector('[data-js="first-input"]');
 const secondInput = document.querySelector('[data-js="second-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+// switch button:
+const switchButton = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
