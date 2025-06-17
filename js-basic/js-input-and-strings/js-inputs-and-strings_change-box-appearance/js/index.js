56adbace54237d9b18ffc2b7ed77c49ac3a25262
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-17 13:24:58 +0200

    add the commands inside event listener

--- js-basic/js-input-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-basic/js-input-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,18 +1,37 @@
 console.clear();
 
-const colorRange = document.querySelector('[data-js="input-color"]');
-const borderRadiusRange = document.querySelector('[data-js="input-radius"]');
-const rotationRange = document.querySelector('[data-js="input-rotation"]');
+const box = document.querySelector('[data-js="box"]');
 
-colorRange.addEventListener("input", () => {});
+const inputColor = document.querySelector('[data-js="input-color"]');
+const inputRadius = document.querySelector('[data-js="input-radius"]');
+const inputRotation = document.querySelector('[data-js="input-rotation"]');
 
-borderRadiusRange.addEventListener("input", () => {});
+// zum prüfen ob bishier alles korrekt angezeigt wird in der console:
+// console.log({
+//   box,
+//   inputColor,
+//   inputRadius,
+//   inputRotation,
+// });
 
-rotationRange.addEventListener("input", () => {});
+inputColor.addEventListener("input", () => {
+  const color = inputColor.value;
+  box.style.background = `hsl(${color}, 70%, 60%)`;
+});
+
+inputRadius.addEventListener("input", () => {
+  const radius = inputRadius.value;
+  box.style.borderRadius = `${radius}%`;
+});
+
+inputRotation.addEventListener("input", () => {
+  const rotation = inputRotation.value;
+  box.style.transform = `rotate(${rotation}deg)`;
+});
 
 // Change the appearance of the box according to the slider values (color, border radius, and rotation).
 
-// [ ] Add an event listener for each input element.
+// [x] Add an event listener for each input element.
 // [ ] When the slider control of inputColor is moved, change the background color of the box. Hint: Use the hsl() function to set the color dynamically.
 // [ ] When the slider control of inputRadius is moved, change the border radius of the box. If the slider is at its maximum, make the box circular.
 // [ ] When the slider control of inputRotation is moved, rotate the box. Hint: Use the transform CSS property with a rotate value in deg.
