622b750e3bcbe0383b45112f1bce4840d8b6305c
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-17 10:33:07 +0200

    add variables for textinput and button

--- js-basic/js-input-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-basic/js-input-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,11 @@
 console.clear();
+
+const textInput = document.querySelector('["first-input"]');
+const buttonUppercase = document.querySelector('["button-uppercase"]');
+
+// Convert the text in the input field to uppercase when the button is clicked.
+
+// [ ] Assign the input element to a variable.
+// [ ] Assign the button element to a variable.
+// [ ] Add an event listener to the button that converts the input value to uppercase when the button is clicked.
+// Hint: Access the input value by using .value on the input element.
