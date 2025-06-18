4385d3564e7b19c7a393ca221c36a337cb26751e
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-18 13:17:12 +0200

    append, add event listeners and added text content

--- js-basic/js-create-element/js-createelement_social-media-post/js/index.js
+++ js-basic/js-create-element/js-createelement_social-media-post/js/index.js
@@ -15,8 +15,23 @@ const article = document.createElement("article");
 const p = document.createElement("p");
 const footer = document.createElement("footer");
 const span = document.createElement("span");
+const newLikeButton = document.createElement("button");
 
 article.classList.add("post");
 p.classList.add("post__content");
 footer.classList.add("post__footer");
 span.classList.add("post__username");
+newLikeButton.classList.add("post__button");
+
+p.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+span.textContent = "@username";
+newLikeButton.textContent = "♥ Like";
+
+newLikeButton.addEventListener("click", handleLikeButtonClick);
+
+const body = document.body;
+
+body.append(article);
+
+article.append(p, footer);
+footer.append(span, newLikeButton);
