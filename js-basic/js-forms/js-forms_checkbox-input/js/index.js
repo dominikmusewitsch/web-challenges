f6d5cfd00972e29899a9bd05b21f6fd67bb890c7
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-19 11:19:16 +0200

    solution

--- js-basic/js-forms/js-forms_checkbox-input/js/index.js
+++ js-basic/js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const finalMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,45 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideFinalMessage() {
+  finalMessage.setAttribute("hidden", "");
+}
+
+function showFinalMessage() {
+  finalMessage.removeAttribute("hidden");
+}
+
+hideTosError();
+hideFinalMessage();
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
 
+  const isChecked = event.target.elements.tos.checked;
+
+  // console.log(formElements.tos.checked);
+
+  if (!isChecked) {
+    showTosError();
+    hideFinalMessage();
+    return;
+  } else {
+    hideTosError();
+    showFinalMessage();
+  }
+
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
+});
+
+tosCheckbox.addEventListener("input", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
 });
