4ba0a3192abc1982ee05b03675ebcc078a1e4004
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-23 15:47:48 +0200

    create li element and append it to ol

--- js-basic/js-loops/js-loops_forof-list/js/index.js
+++ js-basic/js-loops/js-loops_forof-list/js/index.js
@@ -17,4 +17,19 @@ const programmingLanguages = [
 
 // --v-- write or modify code below this line --v--
 
+const li = document.createElement("li");
+ol.append(li);
+
 // --^-- write or modify code above this line --^--
+
+// In ./js/index.js, you'll find:
+
+// An ol element that’s been created and appended to the document.
+// A string array stored in a variable called programmingLanguages.
+// Your task is to create a list item for each array item and add it to the ordered list. Each list item should display the value of the current array item. Use a for...of loop to accomplish this.
+
+// The following hints may guide you:
+
+// Do you remember the property for setting the text of an HTML element?
+// Be sure to append each list item to the ordered list.
+// Switch to the ./js/index.js to begin!
