9060dfc49a236a7c48a631f1ed0f85949fa56453
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-25 12:26:55 +0200

    fetch data from api

--- js-basic/js-fetch/js-fetch_star-wars/js/index.js
+++ js-basic/js-fetch/js-fetch_star-wars/js/index.js
@@ -41,6 +41,9 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.py4e.com/api/people");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
 }
