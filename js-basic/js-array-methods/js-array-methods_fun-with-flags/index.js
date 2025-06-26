65d4ce205ca7b2ca38db4eef9955422019d188c7
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-26 14:47:38 +0200

    add solution to fun with flags

--- js-basic/js-array-methods/js-array-methods_fun-with-flags/index.js
+++ js-basic/js-array-methods/js-array-methods_fun-with-flags/index.js
@@ -9,10 +9,36 @@ queryInput.addEventListener("input", (event) => {
 
   const searchString = event.target.value;
 
-  const foundCountry = null;
+  // const foundCountry = countries.find((country) =>
+  //   country.name.toLowerCase().startsWith(searchString.toLowerCase())
+  // );
 
-  if (foundCountry) {
-    const countryElement = Country(foundCountry);
+  // if (foundCountry) {
+  //   const countryElement = Country(foundCountry);
+  //   container.append(countryElement);
+  // }
+  const foundCountries = countries.filter((country) =>
+    country.name.toLowerCase().startsWith(searchString.toLowerCase())
+  );
+
+  foundCountries.forEach((country) => {
+    const countryElement = Country(country);
     container.append(countryElement);
-  }
+  });
+
+  // if (foundCountries) {
+  //   const countryElement = Country(foundCountry);
+  //   container.append(countryElement);
+  // }
 });
+
+// 2. Find All Matching Countries
+// Change the method from find to filter,
+// to filter the countries to those which name
+// starts with the search string. Remember to change
+// the variable name from foundCountry to foundCountries
+// to reflect that it now is an array.
+
+// Adapt the rendering of the found countries.
+// You'll need to loop over the array of foundCountries.
+// Which array method can you use for that?
