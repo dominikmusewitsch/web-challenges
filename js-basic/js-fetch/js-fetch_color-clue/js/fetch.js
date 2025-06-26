ca7d675a56ac46270542e8bbf9d49da808c44eee
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-26 09:57:29 +0200

    correct code for fetch color clue

--- js-basic/js-fetch/js-fetch_color-clue/js/fetch.js
+++ js-basic/js-fetch/js-fetch_color-clue/js/fetch.js
@@ -19,5 +19,26 @@ export async function fetchNewColor() {
 
   // --v-- your code here --v--
 
+  const response = await fetch(colorApiUrl);
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.name.closest_named_hex);
+  console.log(data.name.value);
+
+  setColorToGuess(data.name.closest_named_hex, data.name.value);
+
   // --^-- your code here --^--
 }
+
+// Check out the js/fetch.js file: There is a fetchNewColor() function which creates a
+// random hex code and creates the correct url to fetch data from the Color API.
+
+// Your task is to implement the fetch functionality:
+
+// Use the fetch() function to get data from the Color API.
+// Use the .json() method to parse the body of the response and convert it into an object.
+// Inspect the parsed object (e.g. by logging it to the console) and find the hex value and name of the closest named color.
+// Call the setColorToGuess() function with the hex value and name of the closest named color.
+// Guess colors! 🎉
+// Switch to the js/fetch.js file and start coding!
