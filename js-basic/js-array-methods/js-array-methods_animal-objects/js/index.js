2ee706d2d609621623c585d29ee27e2a133d59df
dominikmusewitsch <dmusewitsch@gmail.com>
2025-06-26 13:55:13 +0200

    add solution of animal objects

--- js-basic/js-array-methods/js-array-methods_animal-objects/js/index.js
+++ js-basic/js-array-methods/js-array-methods_animal-objects/js/index.js
@@ -78,9 +78,16 @@ const animals = [
 ];
 
 // Hint: Besides the array method, check out the string method `startsWith()`.
-const firstAnimalStartingWithLetterG = null;
+const firstAnimalStartingWithLetterG = animals.find((animal) =>
+  animal.name.startsWith("g")
+);
+console.log(firstAnimalStartingWithLetterG);
 
-const indexOfAnimalWithNameLongerFive = null;
+const indexOfAnimalWithNameLongerFive = animals.findIndex(
+  (animal) => animal.name.length > 5
+);
+
+console.log(indexOfAnimalWithNameLongerFive);
 
 // Note:
 // - Sorting strings is slightly more complicated than sorting numbers.
@@ -91,26 +98,70 @@ const indexOfAnimalWithNameLongerFive = null;
 // Hint: sort() mutates the original array, which is bad.
 // Make sure to use toSorted() instead.
 
-const animalsSortedAlphabetically = null;
+const animalsSortedAlphabetically = animals.toSorted((a, b) => {
+  if (a.name < b.name) return -1;
+  if (a.name > b.name) return 1;
+  return 0;
+});
 
-const animalsSortedByWeightStartingWithLowest = null;
+console.log(animalsSortedAlphabetically);
+
+const animalsSortedByWeightStartingWithLowest = animals.toSorted((a, b) => {
+  if (a.weight < b.weight) return -1;
+  if (a.weight > b.weight) return 1;
+  return 0;
+});
+
+console.log("sortiert nach gewicht", animalsSortedByWeightStartingWithLowest);
 
 // Note:
 // - reverse() mutates the original array (like sort() does), which is bad.
 // Use toReversed() instead (or any other method to get the expected result)
 
-const animalsSortedByWeightReversed = null;
+const animalsSortedByWeightReversed =
+  animalsSortedByWeightStartingWithLowest.toReversed();
 
-const animalWithWeightMoreThanFivehundredExists = null;
+console.log("sortiert von schwer zu leicht", animalsSortedByWeightReversed);
+
+const animalWithWeightMoreThanFivehundredExists = animals.some(
+  (animal) => animal.weight > 500
+);
+
+console.log(animalWithWeightMoreThanFivehundredExists);
 
 // Hint: Filter for Europe first, then check every animal for its weight.
-const allAnimalsInEuropeWeighLessThanOnehundred = null;
+const allAnimalsInEuropeWeighLessThanOnehundred = animals.filter(
+  (animal) => animal.continents.includes("Europe") && animal.weight < 100
+);
+
+console.log(allAnimalsInEuropeWeighLessThanOnehundred);
 
 // Hint: filter + map + reduce
-const weightOfAllAnimalsInAfrica = null;
+const animalsInAfrica = animals.filter((animal) =>
+  animal.continents.includes("Africa")
+);
+console.log(animalsInAfrica);
+
+const onlyWeightOfAfricanAnimals = animalsInAfrica.map(
+  (animal) => animal.weight
+);
+console.log(onlyWeightOfAfricanAnimals);
+
+const weightOfAllAnimalsInAfrica = onlyWeightOfAfricanAnimals.reduce(
+  (acc, len) => acc + len,
+  0
+);
+
+console.log(weightOfAllAnimalsInAfrica);
 
 // Hint: As above, but divided by the number of animals in Africa.
-const averageWeightOfAllAnimalsInAfrica = null;
+const amountOfAfricanAnimals = animalsInAfrica.length;
+console.log(amountOfAfricanAnimals);
+
+const averageWeightOfAllAnimalsInAfrica =
+  weightOfAllAnimalsInAfrica / amountOfAfricanAnimals;
+
+console.log(averageWeightOfAllAnimalsInAfrica);
 
 export {
   firstAnimalStartingWithLetterG,
@@ -123,3 +174,30 @@ export {
   weightOfAllAnimalsInAfrica,
   averageWeightOfAllAnimalsInAfrica,
 };
+
+// Tasks
+// In the ./index.js file, there is an animals array with - guess what?
+// - animals. Each animal is an object with some key-value-pairs.
+
+// Below this array, you will find a couple of variables (e.g. firstAnimalStartingWithLetterG).
+// They all are initialized with null.
+
+// And now it's your turn: delete the value null and write code such that the variables
+// have a value corresponding to their name. Use the following methods to achieve this goal:
+
+// Array.includes()
+// Array.find()
+// Array.findIndex()
+// Array.toSorted()
+// Array.some()
+// Array.every()
+// Array.reduce()
+// Array.toReversed()
+// Additionally, at some point you will need:
+
+// Array.filter()
+// Array.map()
+// String.startsWith()
+// You will find some hints as comments.
+
+// Switch to the ./index.js file and work with animals!
