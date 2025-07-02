1a42fedec60f547f48f53f4f412b228fc981f64e
dominikmusewitsch <dmusewitsch@gmail.com>
2025-07-02 12:00:04 +0200

    add attributes

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,20 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return articleComponent();
+}
+
+function articleComponent() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Hier steht nur Müll</h2>
+      <form>
+        <label htmlFor="eingabe">Eingabefeld</label>
+        <input type="text" id="eingabe"></input>
+      </form>
+      <a href="https://de.wikipedia.org/wiki/Burg" className="article__link">
+        click here to learn smth
+      </a>
+    </article>
+  );
 }
