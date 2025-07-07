96bfff284eeb08b2a5f7a5d58f52e6dfaaf08a56
dominikmusewitsch <dmusewitsch@gmail.com>
2025-07-07 15:19:31 +0200

    fruits solution

--- react-basics/react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
+++ react-basics/react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
@@ -1,5 +1,15 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ color, children }) {
+  return (
+    <p className="card" style={{ backgroundColor: color }}>
+      {children}
+    </p>
+  );
 }
+
+// ## Bonus
+
+// Change the `Card` component so that it receives a `color` prop and set the background-color of the Card to this value. Use the color property of each fruit object for this prop.
+
+// - Hint: check the `./src/components/Card.css` file for some cues as to how to handle having the colors change automatically
