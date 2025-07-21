9f2087514eaabc10d2626a40bf06ee8c01a6f148
dominikmusewitsch <dmusewitsch@gmail.com>
2025-07-21 12:56:25 +0200

    show boxes horizontally

--- react-basics/react-styled-components/react-styled-components_box/pages/index.js
+++ react-basics/react-styled-components/react-styled-components_box/pages/index.js
@@ -1,13 +1,18 @@
+import styled from "styled-components";
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
 import BoxWithStyledComponents from "@/components/BoxWithClassName/BoxWithStyledComponents.js";
 
 export default function HomePage() {
   return (
-    <div>
+    <BoxWrapper>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
       <BoxWithStyledComponents />
       <BoxWithStyledComponents $isBlack />
-    </div>
+    </BoxWrapper>
   );
 }
+
+const BoxWrapper = styled.div`
+  display: flex;
+`;
