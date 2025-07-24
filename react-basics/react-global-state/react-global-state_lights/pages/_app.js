2553c026d59f73435f8f2a3c08afbe20e1e3c1c3
dominikmusewitsch <dmusewitsch@gmail.com>
2025-07-24 12:36:01 +0200

    lights solution

---
+++ react-basics/react-global-state/react-global-state_lights/pages/_app.js
@@ -0,0 +1,59 @@
+import GlobalStyle from "../styles";
+import Layout from "../components/Layout";
+import { useState } from "react";
+
+export default function App({ Component, pageProps }) {
+  const [lights, setLights] = useState([
+    { name: "Livingroom", isOn: false, id: 1 },
+    { name: "Kitchen", isOn: false, id: 2 },
+    { name: "Bedroom", isOn: false, id: 3 },
+    { name: "Bathroom", isOn: false, id: 4 },
+    { name: "Garage", isOn: false, id: 5 },
+    { name: "Porch", isOn: false, id: 6 },
+    { name: "Garden", isOn: false, id: 7 },
+    { name: "Office", isOn: false, id: 8 },
+  ]);
+  const lightsOn = lights.filter((light) => light.isOn === true).length;
+
+  function handleToggle(lightsId) {
+    setLights(
+      lights.map((light) =>
+        light.id === lightsId ? { ...light, isOn: !light.isOn } : light
+      )
+    );
+  }
+
+  function handleToggleAllLightsOn() {
+    setLights(
+      lights.map((light) => ({
+        ...light,
+        isOn: true,
+      }))
+    );
+  }
+
+  function handleToggleAllLightsOff() {
+    setLights(
+      lights.map((light) => ({
+        ...light,
+        isOn: false,
+      }))
+    );
+  }
+
+  const isDimmed = lightsOn > 0 ? false : true;
+
+  return (
+    <Layout isDimmed={isDimmed}>
+      <GlobalStyle />
+      <Component
+        {...pageProps}
+        lights={lights}
+        toggleLight={handleToggle}
+        handleToggleAllLightsOn={handleToggleAllLightsOn}
+        handleToggleAllLightsOff={handleToggleAllLightsOff}
+        lightsOn={lightsOn}
+      />
+    </Layout>
+  );
+}
---
+++ react-basics/react-global-state/react-global-state_lights/pages/index.js
@@ -0,0 +1,18 @@
+import Link from "../components/Link";
+
+export default function HomePage({ lightsOn }) {
+  console.log(lightsOn);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>{lightsOn} light(s) are on.</p>
+      <p>
+        <Link href="/rooms">All Rooms →</Link>
+      </p>
+      <p>
+        <Link href="/actions">Quick Actions →</Link>
+      </p>
+    </div>
+  );
+}
