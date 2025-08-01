7762e2403299584c5b129851157cfbfb4376fe58
dominikmusewitsch <dmusewitsch@gmail.com>
2025-08-01 11:05:09 +0200

    iss tracker solution

--- react-basics/react-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
+++ react-basics/react-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
@@ -11,7 +11,22 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  async function getISSCoords() {
+    const response = await fetch(URL);
+    const info = await response.json();
+
+    setCoords(info);
+  }
+
+  useEffect(() => {
+    getISSCoords();
+
+    const intervalID = setInterval(() => {
+      getISSCoords();
+    }, 5000);
+
+    return () => clearInterval(intervalID);
+  });
 
   return (
     <main>
