5bf8679b28bcb51e499132fc1192e693d539f522
dominikmusewitsch <dmusewitsch@gmail.com>
2025-08-13 16:18:02 +0200

    solution fish shop update and delete

--- backend-basics/backend-update-and-delete/backend-update-and-delete_products/pages/[id].js
+++ backend-basics/backend-update-and-delete/backend-update-and-delete_products/pages/[id].js
@@ -2,12 +2,41 @@ import useSWR from "swr";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import StyledLink from "@/components/Link";
+import { useState } from "react";
+import ProductForm from "@/components/ProductForm";
 
 export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
+
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  async function handleDeleteProduct(id) {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      router.push("/");
+    }
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -24,6 +53,25 @@ export default function Product() {
       <p>
         Price: {data.price} {data.currency}
       </p>
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        EDIT
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          handleDeleteProduct(id);
+        }}
+      >
+        DELETE
+      </button>
+      {isEditMode === true && (
+        <ProductForm onSubmit={handleEditProduct} product={data} />
+      )}
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
   );
